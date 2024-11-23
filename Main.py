7f6c3aaeb4299ac79bb79bb03033594ebf94c57f
MemExplorer <132466484+MemExplorer@users.noreply.github.com>
2024-11-24 03:56:03 +0800

    Added python files

--- Main.py
+++ Main.py
@@ -2,12 +2,5 @@ from flask import Flask, redirect, url_for, request
 from flask import render_template
 app = Flask(__name__, template_folder='webpages')
 
-
 if __name__ == '__main__':
-    app.run(host='192.168.100.9',port='60',debug=True)
-    
-    
-    
-    
-    
-    
+    app.run(debug=True)
